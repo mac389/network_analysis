3744a4a935bfb5700ffa5f48d342189f426fdd33
Nick <newbiedoodle@gmail.com>
2014-03-01 16:17:34 -0500

    Tried to fix/simplify it.
    I'm not entirely sure why this doesn't work. Any help is appreciated.

--- src/compareMatricies.py
+++ src/compareMatricies.py
@@ -1,23 +1,26 @@
 import numpy as np
-import numpy.linalg as la
 
 #Note that the two arrays must be the same size to
 #allow the program to work properly.
 matrix1 = np.eye(5)
 matrix2 = np.eye(5)
-matrix2[1][1] += 1
 
 a, vectors1 = la.eig(matrix1)
 a, vectors2 = la.eig(matrix2)
 
 cosinesBetween = np.zeros(shape=(len(matrix1), len(matrix1)))
 
-for i in range(0, len(vectors1)):
-    print np.dot(vectors1[i],vectors2[i])/la.norm(vectors1[i])/la.norm(vectors2[i])
-    #The above mess was taken from http://stackoverflow.com/a/2827466
+def magnitude(vector):
+    sum = 0
+    for dim in vector:
+        sum += (dim**2)
+    return sum ** (0.5)
 
+for i in range(0, len(vectors1)):
+    print np.dot(vectors1[i],vectors2[i])/(magnitude(vectors1[i])*la.norm(vectors2[i]))
 
 #For some reason, the implementation of Python I'm using apparently considers the two
-#matricies to be equal, even if I use the "==" operator builtin. No idea why.
-#  (Note: Based on the output, I assume that "==" compares each element
+#matrices to be equal, even if I use the "==" operator builtin. No idea why.
+#  (I had manually set the second array to be the identity matrix times 2)
+#  (Note: Based on the output, I had assumed that "==" compares each element
 #   element individually. Is this true?)
