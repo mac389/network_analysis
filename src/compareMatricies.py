0662fe227c88fd2297008f8e474ec55d8d18f1ce
Michael Chary <mac389@gmail.com>
2014-03-02 12:32:14 -0500

    updated comparematrices so that it creates a graph

--- src/compareMatricies.py
+++ src/compareMatricies.py
@@ -1,26 +1,55 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+from numpy import linalg as la
+from pprint import pprint
+from matplotlib import rcParams
+
+rcParams['text.usetex'] = True
 #Note that the two arrays must be the same size to
 #allow the program to work properly.
-matrix1 = np.eye(5)
-matrix2 = np.eye(5)
 
-a, vectors1 = la.eig(matrix1)
-a, vectors2 = la.eig(matrix2)
+format = lambda text: r'\Large \textbf{%s}'%text
 
-cosinesBetween = np.zeros(shape=(len(matrix1), len(matrix1)))
+ndims = 5
+repeats = 2
 
-def magnitude(vector):
-    sum = 0
-    for dim in vector:
-        sum += (dim**2)
-    return sum ** (0.5)
+weights = 0
+vectors = 1
 
-for i in range(0, len(vectors1)):
-    print np.dot(vectors1[i],vectors2[i])/(magnitude(vectors1[i])*la.norm(vectors2[i]))
+matrices = [np.eye(ndims)] * repeats
 
-#For some reason, the implementation of Python I'm using apparently considers the two
-#matrices to be equal, even if I use the "==" operator builtin. No idea why.
-#  (I had manually set the second array to be the identity matrix times 2)
-#  (Note: Based on the output, I had assumed that "==" compares each element
-#   element individually. Is this true?)
+
+def similarity(matrices, visualize=False):
+	#Assume input is a list of matrices
+	eigendecomposition = map(la.eig,matrices)
+	answer = np.zeros((ndims,ndims,len(eigendecomposition),len(eigendecomposition)))
+	for i in xrange(len(eigendecomposition)):
+		for j in xrange(i):
+
+			one = eigendecomposition[i][vectors]
+			two = eigendecomposition[j][vectors]
+			
+			answer[:,:,i,j] = one.dot(two) 
+
+			if visualize:
+
+				fig = plt.figure()
+				ax = fig.add_subplot(111)
+
+				cax = ax.imshow(answer[:,:,i,j],aspect='auto',interpolation='nearest')
+				cbar = plt.colorbar(cax)
+
+				ax.set_ylabel(r'\Large \textbf{Eigenvectors of matrix %d}'%i)
+				ax.set_xlabel(r'\Large \textbf{Eigenvectors of matrix %d}'%j)
+				cbar.set_label(r'\Large $\mathbf{\cos \; \theta}$', rotation='horizontal')
+
+				plt.tight_layout()
+				plt.savefig('comparison-%d-%d.png'%(i,j),dpi=200)
+	return answer
+
+def angle(one,two):
+	return np.dot(one,two)/float(la.norm(one)*la.norm(two))
+
+
+similarities = similarity(matrices, visualize=True)
